b746c9c0f7d8f0b1b0c2ae3322e7ab9dcdcc316e
AnddyAnddy <anddyhbc@gmail.com>
2022-02-09 23:42:30 +0100

    fix bug

--- src/modules/data.py
+++ src/modules/data.py
@@ -124,17 +124,17 @@ class Data:
             all_teams = teams["div1"] + teams["div2"]
         try:
             score = [line for line in infos if any(team in line for team in all_teams)][0]
-            only_words = re.split("\W+| ", score)
-            team1 = [x for x in only_words if x in all_teams][0].strip()
-            team2 = [x for x in reversed(only_words) if x in all_teams][0].strip()
-            if team1 == team2 or team1 not in all_teams or team2 not in all_teams:
-                raise Exception()
             score = re.split(r" +(\d+).*(\d+) +", score)
-            args_score = team1, int(score[1]), int(score[2]), team2
+            for i in range(len(score)):
+                for team in teams:
+                    if team in score[i]:
+                        score[i] = team
+            args_score = score[0], int(score[1]), int(score[2]), score[3]
             self._data["score"] = {args_score[0]: args_score[1], args_score[3]: args_score[2]}
             self._data["title"] = f"{args_score[0]} {args_score[1]} - {args_score[2]} {args_score[3]}"
             self._data["div"] = 1 if args_score[0] in teams["div1"] else 2
         except Exception:
+            raise
             self._data["score"] = "Unknown"
             self._data["title"] = "Unknown"
             self._data["div"] = 0
